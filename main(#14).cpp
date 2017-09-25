faa4aa918ea0fb10be70a3654d4866ad11a2211d
Jake J <31711821+RakeYohnshair@users.noreply.github.com>
2017-09-24 21:50:56 -0400

    Update main(#14).cpp

--- main(#14).cpp
+++ main(#14).cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 using namespace std;
 
@@ -10,11 +9,14 @@ int main()
   cin >> t1;
   cout << "Enter end time (0000-2359): " ;
   cin >> t2;
+  if ( t2 < t1 )
+  {
+    t2 += 2400 ;
+  }
   int t1_mins = (t1 / 100) * 60 + (t1 % 100); //converts t1 to Minutes
   int t2_mins = (t2 / 100) * 60 + (t2 % 100); //converts t2 to Minutes
   int tdif = (t2_mins - t1_mins); // defines time difference and finds time difference
-  if ( t2 < t1 ) //tells what to do when start time is later in the day then end time
-  t2_mins += 24 * 60;
+  
   cout << tdif <<" Minutes Ahead" ; 
       return 0;
 }
