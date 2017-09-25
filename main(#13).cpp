7551d87d453be479791a3618c1b3a7120a010322
Jake J <31711821+RakeYohnshair@users.noreply.github.com>
2017-09-24 20:09:17 -0400

    Create main(#13).cpp

---
+++ main(#13).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+ 
+double ConvertToMPH (int paceInMin, int paceInSec);
+double ConvertToMPH (double kph);
+ 
+int main()
+{
+ //declaring
+  double paceInMin;
+  double paceInSec;
+  double kph;
+ 
+  cout << "enter pace in minutes: " ;
+  cin >> paceInMin;  //enter your pace in minutes
+  cout << "enter pace in seconds: " ;
+  cin >> paceInSec;  //enter your pace in seconds
+  cout << "pace in MPH is: " << ConvertToMPH (paceInMin, paceInSec) <<  endl; //cout pace conversion
+ 
+  cout << "enter speed in KPH: " ;
+  cin >> kph; //enter kph
+  cout << "pace in MPH is: " << ConvertToMPH (kph) << endl; //cout kph to mph
+ 
+  return 0;
+}
+ 
+double ConvertToMPH(int paceInMin, int paceInSec) //pace conversions
+{
+    return 60/(paceInMin + paceInSec/60.);
+}
+ 
+double ConvertToMPH(double kph) //MPH to Kph conversion
+{
+    return kph/1.61;
+}
